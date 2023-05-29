8182a2da896cffc6cd06bc84988c593966881837
shpater1234 <shpaters@hotmail.com>
2023-05-29 17:44:03 +0300

    Add project files.

---
+++ Ex04.Menus.Delegates/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex04.Menus.Delegates
+{
+    internal class MainMenu
+    {
+        MenuItem m_MainMenu = new MenuItem();
+
+        public void stam()
+        {
+            m_MainMenu.MenuMethod += firstMenuFunc();
+        }
+
+        public void fisrtMenuFunc()
+        {
+
+        }
+    }
+}
---
+++ Ex04.Menus.Delegates/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ex04.Menus.Delegates
+{
+    internal class MenuItem
+    {
+        string m_Name;
+        List<MenuItem> m_SubMenus;
+        Action MenuMethod;
+        MenuItem m_PreviousMenu;
+
+
+
+    }
+}
